daa094e5091116e44576f570af20ca9ce12707ff
LiuTeng <lewtonstein@gmail.com>
2019-09-08 17:02:48 +0200

    ..

---
+++ bin/pl_VT.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import os,sys
+import numpy as np
+import pylab as pl
+import json
+__doc__="""
+Input the *_VT.dat file from slvt.py and optionally:
+	the *_ctd.dat file from slvt.py,
+	or "-s", which means plotting step by step.
+"""
+
+if '-s' in sys.argv:
+	sys.argv.remove('-s')
+	Step=True
+	pl.ion()
+else: Step=False
+
+VTfile=sys.argv[1]
+with open(VTfile,'r') as fin:
+	l=fin.readline()
+	h=json.loads(l.strip('#'))
+	d=np.loadtxt(fin)
+fig,ax=pl.subplots()
+pl.xlim(h['xlow'],h['xhigh'])
+pl.ylim(h['ylow'],h['yhigh'])
+ax.set(adjustable='box', aspect='equal')
+for l in d:
+	if Step:
+		input('Enter to continue')
+		pl.draw()
+	pl.plot([l[1],l[3]],[l[2],l[4]],'b-')
+	pl.plot([l[5],l[7]],[l[6],l[8]],'ro')
+if Step: pl.ioff()
+if len(sys.argv)>2 and os.path.isfile(sys.argv[2]): 
+	ctd=np.loadtxt(sys.argv[2])
+	for l in ctd:
+		pl.plot([l[1],l[3]],[l[2],l[4]],'g-')
+pl.savefig(VTfile.rsplit('.')[0]+'.png',bbox_inches='tight')
+print('>> '+VTfile.rsplit('.')[0]+'.png')
+pl.tight_layout()
+pl.show()
